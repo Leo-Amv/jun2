898fc4de23f3c6679e27ea3aadc6a90af0f385f6
Leo <leonidamvrosov@yandex.ru>
2023-12-13 21:44:44 +0500

    hw added

---
+++ src/main/java/ru/gb/AfterEach.java
@@ -0,0 +1,7 @@
+package ru.gb;
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AfterEach {
+}
---
+++ src/main/java/ru/gb/Skip.java
@@ -0,0 +1,10 @@
+package ru.gb;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface Skip {
+
+}
